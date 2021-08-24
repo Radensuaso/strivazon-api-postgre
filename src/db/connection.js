28745766eb1e311d6430ad8c1fca17e759335df4
Radensuaso <andrels9283@gmail.com>
2021-08-24 14:53:18 +0100

    tables were created

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,15 +3,18 @@ import pg from "pg";
 // for connection information
 const { Pool } = pg;
 
-console.log(process.env.NODE_ENV);
+const sslConfig =
+  process.env.NODE_ENV !== "development"
+    ? {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
 const db = new Pool({
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  connectionString:
-    process.env.NODE_ENV !== "development"
-      ? process.env.DATABASE_URL
-      : process.env.DATABASE_URL_DEV,
+  connectionString: process.env.DATABASE_URL,
+  ...sslConfig,
 });
 
 export default db;
